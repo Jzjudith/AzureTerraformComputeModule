2ec883c5d35b46a987c11806a3325c1b50df1635
jzjudith <jzjudith@github.com>
2022-10-13 15:30:39 +0100

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_public_ip" "example" {
   name                = var.public_ip_name
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 
   tags = var.tags
 }
